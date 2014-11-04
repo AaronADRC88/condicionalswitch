4ece62c04cafadb2f2f4e2fc43b7ca06f82e5105
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-11-04 09:05:47 +0100

    facendoo

---
+++ src/condicionalswitch/Condicionalswitch.java
@@ -0,0 +1,19 @@
+
+package condicionalswitch;
+
+public class Condicionalswitch {
+
+    public static void main(String[] args) {
+    String opcion ="un";
+    switch(opcion){
+        case "un": System.out.println("un");
+            break;
+        case "dous": System.out.println("dous");
+            break;
+        case "tres": System.out.println("tres");
+            break;
+        default : System.out.println("opcion non valida");
+    }
+    }
+    
+}
